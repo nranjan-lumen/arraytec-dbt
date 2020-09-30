20169b5df64b599da427c21473cc7022348ddf79
Nitesh Ranjan <nitesh.ranjan@lumen.com>
2020-09-30 13:03:35 +0000

    Has error

---
+++ models/demo/customer_stage.sql
@@ -0,0 +1,20 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+stg_customer as (
+
+    select
+	C_CUSTKEY,
+	C_NAME,
+	C_ADDRESS,
+	C_MKTSEGMENT,
+	C_NATIONKEY
+
+    from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."CUSTOMER"
+
+)
+
+select * from stg_customer
+
